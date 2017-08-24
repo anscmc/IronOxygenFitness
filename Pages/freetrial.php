1469d9fe7298c0b11cedaa224f33c3eae4efee03
Andrew McCracken <Andrew McCracken>
2017-08-23 21:33:12 -0400

    updates 8/23/17

---
+++ Pages/freetrial.php
@@ -0,0 +1,56 @@
+<?
+	$form_id = 2;
+	require "../form/submit.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Free Trial | Iron Oxygen Fitness</title>
+        <meta name="description" content="Fill out the form to receive your free week pass to Iron Oxygen Gym!">
+        <meta name="keywords" content="free, trial, pass, membership">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <link rel="stylesheet" type="text/css" href="../Resources/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="../Resources/css/animate.css">
+        <link rel="stylesheet" type="text/css" href="../Resources/css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,300italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+
+
+    </head>
+    <body>
+
+      <header class="trialHeader">
+          
+            <a href="../index.html"><img alt="iron oxygen fitness gym" class="animated slideInDown" src="../Resources/css/img/IOfull.png" style="width:40%;max-width:300px;margin-top:5px;margin-left:5px;"></a>
+
+
+     <div style="border-top:.1px solid white;width:75%;max-width:400px;margin:0 auto;margin-top:5px;">
+     <br>
+     <p style="color:white;text-align:left;">Fill out the form below to receive your free week pass to Iron Oxygen Fitness! Redeem by showing the text or email you receive at the front desk. POWER ON!</p>
+	 
+		<?
+		
+			require "../form/form.php";
+		
+		?>
+
+	 
+	 
+	 </div>
+         <div style="font-size:12px;font-weight:100 !important;padding-top:5px;color:white">
+             Copyright &copy; 2017 Iron Oxygen Fitness - <a style="text-decoration:underline" target="_blank" href="terms.html">Terms of Service and Privacy Policy</a>
+         </div>
+      </header>
+
+
+      
+    </body>  
+</html>
